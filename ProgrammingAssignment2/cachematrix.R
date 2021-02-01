01dcff509ec63f6db15ff316fdd8bd9da4eb5115
Gabriel Miranda <63483031+Gabbro29@users.noreply.github.com>
2021-02-01 19:38:42 -0300

    Add files via upload

---
+++ ProgrammingAssignment2/cachematrix.R
@@ -0,0 +1,36 @@
+## This functions are meant to make a cache copy of the matrix X
+## 
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+    ma<-NULL ## set to NULL the initializing matrix
+    set<-function(y){  ## set the cache values to null
+        x<<-y
+        ma<<-NULL
+    }
+    get<-function()x
+    setmatrix<-function(matrix) ma<<-matrix  ## this three functions get us the values
+    getmatrix<-function() ma
+    list( set = set , 
+          get=get, 
+          setmatrix=setmatrix,
+          getmatrix=getmatrix)
+    
+}
+
+
+## This functions difines the inverse matrix of the matrix x
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x' x being a list returned by makeCacheMatrix
+    ma<- x$getmatrix()
+    if (!is.null(ma)){  ## if it's in the cache does this
+        message("getting cache data")
+        return(ma)
+    }
+    data<-x$get() ## if not creates a new one on the cache
+    ma<-solve(data)
+    x$setmatrix(ma)
+    ma
+}
